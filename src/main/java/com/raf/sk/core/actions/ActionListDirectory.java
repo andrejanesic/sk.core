83267d8806296183223d280ae0fa04ac46851b41
Andreja Nesic <andreja.nesic@brerasolutions.com>
2021-11-19 16:12:48 +0100

    dodata ActionListDirectory komanda

---
+++ src/main/java/com/raf/sk/core/actions/ActionListDirectory.java
@@ -0,0 +1,66 @@
+package com.raf.sk.core.actions;
+
+import com.raf.sk.core.config.IConfig;
+import com.raf.sk.core.core.Core;
+import com.raf.sk.core.exceptions.IActionBadParameterException;
+import com.raf.sk.core.exceptions.IActionInsufficientPrivilegeException;
+import com.raf.sk.core.exceptions.IComponentNotInitializedException;
+import com.raf.sk.core.exceptions.INodeRootNotInitializedException;
+import com.raf.sk.core.repository.Directory;
+import com.raf.sk.core.repository.File;
+import com.raf.sk.core.repository.INode;
+import com.raf.sk.core.storage.StorageManager;
+import com.raf.sk.core.user.PrivilegeType;
+
+/**
+ * Korisnička akcija čitanja sadržaja direktorijuma zadatog putanjom.
+ *
+ * @see com.raf.sk.core.user.User
+ * @see com.raf.sk.core.repository.Directory
+ */
+public class ActionListDirectory implements IAction {
+
+    /**
+     * Putanja za izlistavanje.
+     */
+    private String path;
+
+    /**
+     * Podrazumevani konstruktor.
+     *
+     * @param path Nova putanja CWD.
+     */
+    public ActionListDirectory(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public Directory run() {
+        if (Core.getInstance().ConfigManager().getConfig() == null)
+            throw new IComponentNotInitializedException(IConfig.class);
+
+        if (Core.getInstance().StorageManager().getRoot() == null)
+            throw new IComponentNotInitializedException(StorageManager.class);
+
+        //noinspection ConstantConditions
+        if (!(Core.getInstance().UserManager().getUser().hasPrivilege(path, PrivilegeType.INODE_READ) ||
+                Core.getInstance().UserManager().getUser().hasPrivilege(PrivilegeType.INODE_ALL) ||
+                Core.getInstance().UserManager().getUser().hasPrivilege(PrivilegeType.ALL)))
+            throw new IActionInsufficientPrivilegeException();
+
+        try {
+            //noinspection ConstantConditions
+            INode targetNode = Core.getInstance().UserManager().getUser().getCwd().resolvePath(path);
+            if (targetNode instanceof File)
+                throw new IActionBadParameterException("The path you specified is a file.");
+            return (Directory) targetNode;
+        } catch (INodeRootNotInitializedException e1) {
+            throw new IActionBadParameterException("The path you specified is not valid.");
+        }
+    }
+
+    @Override
+    public Object undo() {
+        return run();
+    }
+}
